31f43622a7ba9fca512d13e2449945815dbb0f23
aude <aude.wiki@gmail.com>
2016-04-28 16:51:38 -0400

    Add populateSitesTable and related code

---
+++ tests/phpunit/SiteMatrixParserTest.php
@@ -0,0 +1,225 @@
+<?php
+
+namespace Wikimedia\Sites\Test;
+
+use MediaWikiSite;
+use PHPUnit_Framework_TestCase;
+use Wikimedia\Sites\SiteMatrixParser;
+
+/**
+ * @covers Wikimedia\Sites\SiteMatrixParser
+ *
+ * @group WikimediaSItes
+ *
+ * @license GPL-2.0+
+ * @author Katie Filbert < aude.wiki@gmail.com >
+ */
+class SiteMatrixParserTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * @dataProvider sitesFromJsonProvider
+	 */
+	public function testSitesFromJson( $scriptPath, $articlePath, $protocol, $expected ) {
+		$json = $this->getSiteMatrixJson();
+
+		$siteMatrixParser = new SiteMatrixParser(
+			$scriptPath,
+			$articlePath,
+			$protocol
+		);
+
+		$sites = $siteMatrixParser->sitesFromJson( $json );
+
+		ksort( $expected );
+		ksort( $sites );
+
+		$this->assertEquals( $expected, $sites );
+	}
+
+	public function sitesFromJsonProvider() {
+		$siteData = $this->getSiteData();
+
+		$data = array();
+
+		$data['Protocol relative'] = array(
+			'/w/$1',
+			'/wiki/$1',
+			false,
+			$this->getSites( $siteData, '/w/$1', '/wiki/$1' )
+		);
+
+		$data['Keep the protocol'] = array(
+			'/w/$1',
+			'/wiki/$1',
+			true,
+			$this->getSites( $siteData, '/w/$1', '/wiki/$1', 'http:' )
+		);
+
+		$data['Force a protocol'] = array(
+			'/w/$1',
+			'/wiki/$1',
+			'CompuGlobalHyperMegaNet',
+			$this->getSites( $siteData, '/w/$1', '/wiki/$1', 'CompuGlobalHyperMegaNet:' )
+		);
+
+		return $data;
+	}
+
+	protected function getSiteMatrixJson() {
+		$sites = array(
+			array(
+				'code' => 'en',
+				'name' => 'English',
+				'site' => array(
+					array(
+						'url' => 'http://en.wikipedia.org',
+						'dbname' => 'enwiki',
+						'code' => 'wiki',
+						'sitename' => 'Wikipedia'
+					),
+					array(
+						'url' => 'http://en.wikivoyage.org',
+						'dbname' => 'enwikivoyage',
+						'code' => 'wikivoyage',
+						'sitename' => 'Wikipedia'
+					),
+					array(
+						'url' => 'http://en.wikiquote.org',
+						'dbname' => 'enwikiquote',
+						'code' => 'wikiquote',
+						'sitename' => 'Wikipedia'
+					)
+				),
+			),
+			array(
+				'code' => 'fr',
+				'name' => 'français',
+				'site' => array(
+					array(
+						'url' => 'http://fr.wikipedia.org',
+						'dbname' => 'frwiki',
+						'code' => 'wiki',
+						'sitename' => 'Wikipedia'
+					),
+					array(
+						'url' => 'http://fr.wikivoyage.org',
+						'dbname' => 'frwikivoyage',
+						'code' => 'wikivoyage',
+						'sitename' => 'Wikipedia'
+					)
+				)
+			)
+		);
+
+		$specialSites = array();
+
+		$specialSites[] = array(
+			'url' => 'http://commons.wikimedia.org',
+			'dbname' => 'commonswiki',
+			'code' => 'commons'
+		);
+
+		$specialSites[] = array(
+			'url' => 'http://www.wikidata.org',
+			'dbname' => 'wikidatawiki',
+			'code' => 'wikidata'
+		);
+
+		$specials = array(
+			'specials' => $specialSites
+		);
+
+		$siteMatrix = array_merge(
+			array( 'count' => 879 ),
+			$sites,
+			$specials
+		);
+
+		$data = array(
+			'sitematrix' => $siteMatrix
+		);
+
+		return json_encode( $data );
+	}
+
+	protected function getSiteData() {
+		$siteData = array(
+			array(
+				'siteid' => 'enwiki',
+				'group' => 'wikipedia',
+				'url' => 'en.wikipedia.org',
+				'lang' => 'en'
+			),
+			array(
+				'siteid' => 'frwiki',
+				'group' => 'wikipedia',
+				'url' => 'fr.wikipedia.org',
+				'lang' => 'fr'
+			),
+			array(
+				'siteid' => 'enwikivoyage',
+				'group' => 'wikivoyage',
+				'url' => 'en.wikivoyage.org',
+				'lang' => 'en'
+			),
+			array(
+				'siteid' => 'frwikivoyage',
+				'group' => 'wikivoyage',
+				'url' => 'fr.wikivoyage.org',
+				'lang' => 'fr'
+			),
+			array(
+				'siteid' => 'enwikiquote',
+				'group' => 'wikiquote',
+				'url' => 'en.wikiquote.org',
+				'lang' => 'en'
+			),
+			array(
+				'siteid' => 'commonswiki',
+				'group' => 'commons',
+				'url' => 'commons.wikimedia.org',
+				'lang' => 'en'
+			),
+			array(
+				'siteid' => 'wikidatawiki',
+				'group' => 'wikidata',
+				'url' => 'www.wikidata.org',
+				'lang' => 'en'
+			)
+		);
+
+		return $siteData;
+	}
+
+	public function getSites( array $sitesData, $scriptPath, $articlePath, $protocol = '' ) {
+		$sites = array();
+
+		foreach ( $sitesData as $siteData ) {
+			$fields = array(
+				'globalid' => $siteData['siteid'],
+				'type' => 'mediawiki',
+				'group' => $siteData['group'],
+				'source' => 'local',
+				'language' => $siteData['lang'],
+				'localids' => array(),
+				'internalid' => null,
+				'data' => array(
+					'paths' => array(
+						'file_path' => $protocol . '//' . $siteData['url'] . $scriptPath,
+						'page_path' => $protocol . '//' . $siteData['url'] . $articlePath
+					)
+				),
+				'forward' => false,
+				'config' => array()
+			);
+
+			$site = new MediaWikiSite();
+			$site->unserialize( serialize( $fields ) );
+			$siteId = $siteData['siteid'];
+			$sites[$siteId] = $site;
+		}
+
+		return $sites;
+	}
+
+}
---
+++ tests/phpunit/SitesBuilderTest.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace Wikimedia\Sites\Test;
+
+use HashSiteStore;
+use MediaWikiSite;
+use PHPUnit_Framework_TestCase;
+use SiteList;
+use Wikimedia\Sites\SitesBuilder;
+
+/**
+ * @covers Wikimedia\Sites\SitesBuilder
+ *
+ * @group WikimediaSites
+ *
+ * @license GPL-2.0+
+ * @author Katie Filbert < aude.wiki@gmail.com >
+ */
+class SitesBuilderTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * @dataProvider buildSitesProvider
+	 */
+	public function testBuildSites( array $sites, $group, $wikiId, array $expected ) {
+		$store = new HashSiteStore();
+
+		$validGroups = array( 'wikipedia', 'wikivoyage', 'wikiquote', 'wiktionary',
+			'wikibooks', 'wikisource', 'wikiversity', 'wikinews' );
+
+		$sitesBuilder = new SitesBuilder( $store, $validGroups );
+		$sitesBuilder->buildStore( $sites, $group, $wikiId );
+
+		$expectedSiteList = new SiteList( $expected );
+
+		$this->assertEquals( $expectedSiteList, $store->getSites() );
+	}
+
+	public function buildSitesProvider() {
+		$sitesData = $this->getSitesData();
+		$sites = $this->getSites( $sitesData );
+		$expectedSites = $sites;
+
+		foreach ( $expectedSites as $site ) {
+			if ( $site->getGroup() === 'wikipedia' ) {
+				$site->addInterwikiId( $site->getLanguageCode() );
+				$site->addNavigationId( $site->getLanguageCode() );
+			}
+		}
+
+		$data = array();
+
+		$data[] = array( $sites, 'wikidata', null, $expectedSites );
+		$data[] = array( $sites, 'commons', null, $expectedSites );
+		$data[] = array( $sites, 'wikipedia', null, $expectedSites );
+		$data[] = array( $sites, null, 'enwiki', $expectedSites );
+		$data[] = array( $sites, null, 'commonswiki', $expectedSites );
+
+		$expectedSites2 = $sites;
+
+		foreach ( $expectedSites2 as $site ) {
+			if ( $site->getGroup() === 'wikivoyage' ) {
+				$site->addInterwikiId( $site->getLanguageCode() );
+				$site->addNavigationId( $site->getLanguageCode() );
+			}
+		}
+
+		$data[] = array( $sites, 'wikivoyage', null, $expectedSites2 );
+		$data[] = array( $sites, null, 'enwikivoyage', $expectedSites2 );
+		$data[] = array( $sites, 'wikivoyage', 'enwiki', $expectedSites2 );
+
+		$data[] = array( $sites, 'kittens', null, $sites );
+		$data[] = array( $sites, 'kittens', 'enwiki', $sites );
+		$data[] = array( $sites, null, 'kittenswiki', $sites );
+
+		return $data;
+	}
+
+	private function getSitesData() {
+		$sitesData = array(
+			array(
+				'siteid' => 'enwiki',
+				'group' => 'wikipedia',
+				'url' => 'en.wikipedia.org',
+				'lang' => 'en'
+			),
+			array(
+				'siteid' => 'dewiki',
+				'group' => 'wikipedia',
+				'url' => 'de.wikipedia.org',
+				'lang' => 'de'
+			),
+			array(
+				'siteid' => 'enwikivoyage',
+				'group' => 'wikivoyage',
+				'url' => 'en.wikivoyage.org',
+				'lang' => 'en'
+			),
+			array(
+				'siteid' => 'frwikivoyage',
+				'group' => 'wikivoyage',
+				'url' => 'fr.wikivoyage.org',
+				'lang' => 'fr'
+			),
+			array(
+				'siteid' => 'enwikiquote',
+				'group' => 'wikiquote',
+				'url' => 'en.wikiquote.org',
+				'lang' => 'en'
+			),
+			array(
+				'siteid' => 'commonswiki',
+				'group' => 'commons',
+				'url' => 'commons.wikimedia.org',
+				'lang' => 'en'
+			),
+			array(
+				'siteid' => 'wikidatawiki',
+				'group' => 'wikidata',
+				'url' => 'www.wikidata.org',
+				'lang' => 'en'
+			),
+		);
+
+		return $sitesData;
+	}
+
+	/**
+	 * @param array[] $sitesData
+	 *
+	 * @return MediaWikiSite[]
+	 */
+	private function getSites( array $sitesData ) {
+		$sites = array();
+
+		foreach ( $sitesData as $siteData ) {
+			$fields = array(
+				'globalid' => $siteData['siteid'],
+				'type' => 'mediawiki',
+				'group' => $siteData['group'],
+				'source' => 'local',
+				'language' => $siteData['lang'],
+				'localids' => array(),
+				'internalid' => null,
+				'data' => array(
+					'paths' => array(
+						'file_path' => '//' . $siteData['url'] . '/w/$1',
+						'page_path' => '//' . $siteData['url'] . '/wiki/$1'
+					)
+				),
+				'forward' => false,
+				'config' => array()
+			);
+
+			$site = new MediaWikiSite();
+			$site->unserialize( serialize( $fields ) );
+			$siteId = $siteData['siteid'];
+			$sites[$siteId] = $site;
+		}
+
+		return $sites;
+	}
+
+}
