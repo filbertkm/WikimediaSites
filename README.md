3bce478cfc138ea8bbedf3b06adb018527c8521a
aude <aude.wiki@gmail.com>
2016-04-28 16:29:16 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+WikimediaSites
+===============
+
+Manage Wikimedia sites in MediaWiki's SiteStore.
+
+Includes script to populate the SiteStore based on data from
+a site matrix api.
---
+++ WikimediaSites.i18n.alias.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Aliases for special pages of the WikimediaSites extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+// @codingStandardsIgnoreFile
+
+$specialPageAliases = array();
+
+/** English (English) */
+$specialPageAliases['en'] = array();
